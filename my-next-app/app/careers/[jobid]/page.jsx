7c78ba78952ef307328a764d7534d8d1b64e49db
nidinbose <nidinbose700@gmail.com>
2025-06-17 21:51:53 +0530

    addings

--- my-next-app/app/careers/[jobid]/page.jsx
+++ my-next-app/app/careers/[jobid]/page.jsx
@@ -64,7 +64,7 @@ export default function CareerForm() {
           type="submit"
           className="bg-blue-600 text-white px-4 py-2 rounded"
         >
-          Submit
+          Submit good
         </button>
       </form>
       {message && <p className="mt-4 text-green-700">{message}</p>}
